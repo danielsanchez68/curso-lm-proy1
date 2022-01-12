f426d493507576d7618ec398242bd08a87d3870c
usuario1 <usuario1@gmail.com>
2022-01-12 17:43:31 -0300

    funcion de multiplicacion y su prueba disponible

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const suma = (a,b) => a + b
 const resta = (a,b) => a - b
+const mult = (a,b) => a * b
 
 let a = 6, b = 7
 console.log(`La suma de ${a} más ${b} es ${suma(a,b)}`)
 console.log(`La resta de ${a} menos ${b} es ${resta(a,b)}`)
+console.log(`La multiplicación de ${a} por ${b} es ${mult(a,b)}`)
